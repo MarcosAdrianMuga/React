740dcabe565871f2081525d7b430a941abee23d7
Marcos Muga <marcosadrianmuga@gmail.com>
2023-08-07 18:46:07 -0300

    Lo que hicimos hoy

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,38 @@ export default function Saludo () {
   const [apellido, setApellido] = useState("apellido secreto")
   const [ayudita, setAyudita] = useState(false)
 
+
+
+  const [usuario, setUsuario] = useState({
+    mail: "",
+    constra: ""
+  })
+  const handleRegistroTrucho = () => {
+    console.log(usuario)
+    setUsuario({mail:"", constra: ""})
+  }
+
+  const handleCambio = (e : any) => {
+    if(e.target.name === "mail") {
+      setUsuario({...usuario, mail: e.target.value})
+    } else {
+      setUsuario({...usuario, constra: e.target.value})
+    }
+    
+  }
+
   return(
     <div>
     <Titulo/>
     <h1>Mi nombre es {nombre} {ayudita ? <>{apellido}</> : null}</h1>
-    <button onClick={() => setAyudita(true)}>Mostrar apellido secreto</button>
+    <button onClick={() => {setAyudita(true)}}>Mostrar apellido secreto</button>
     <button onClick={() => setAyudita(false)}>Dejar de mostrar apellido super secreto</button>
+
+    <h1>Formulario! :O</h1>
+    <p>Mail:</p><input name="mail" value={usuario.mail} onChange={(e) => handleCambio(e)}></input>
+    <p>Contraaaaa:</p><input value={usuario.constra} onChange={(e) => handleCambio(e)}></input>
+    <button onClick={() => handleRegistroTrucho()}>Registro trucho</button>
+    
     <Boton/>
     </div>
   )
